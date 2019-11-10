43416bd3b3619d4758518ec6e8168405df9b2134
lloe <56943946+lloe@users.noreply.github.com>
2019-11-10 04:48:27 -0600

    Update createquestions.php

--- Website/createquestions.php
+++ Website/createquestions.php
@@ -1,15 +1,13 @@
-
 <?php
-echo "whoami";
-$question = $_GET["question"];
-$answera = $_GET["answera"];
-$answerb = $_GET["answerb"];
-$answerc = $_GET["answerc"];
-$answerd = $_GET["answerd"];
-$answere = $_GET["answere"];
-$answer = $_GET["answer"];
+$question = $_GET["ques"];
+$answera = $_GET["ansa"];
+$answerb = $_GET["ansb"];
+$answerc = $_GET["ansc"];
+$answerd = $_GET["ansd"];
+$answere = $_GET["anse"];
+$answer = $_GET["ans"];
 
-$questions = fopen("questions.txt", "w");
+$questions = fopen("questions.txt", "a");
 fwrite($questions,$question);
 fwrite($questions,$answera);
 fwrite($questions,$answerb);
@@ -17,7 +15,7 @@ fwrite($questions,$answerc);
 fwrite($questions,$answerd);
 fwrite($questions,$answere);
 
-$key = fopen("key.txt", "w");
+$key = fopen("key.txt", "a");
 fwrite($key,$answer);
 
 fclose($questions);
