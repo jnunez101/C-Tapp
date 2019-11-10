0b450dd240822d063cb8390c9abf540f1b9742cf
lloe <56943946+lloe@users.noreply.github.com>
2019-11-10 10:58:47 -0600

    Update endquiz.php

--- Website/endquiz.php
+++ Website/endquiz.php
@@ -1,7 +1,7 @@
 <?php
 unlink("questions.txt");
 unlink("stuquestions.txt");
-$trigger=fopen("trigger.txt");
+$trigger=fopen("trigger.txt","w");
 fclose($trigger);
 header("location:teacherpage.php")
 ?>
