ef52e9723bfe36b7a2c7c2eccf6a1c8d2b3f8b70
lloe <56943946+lloe@users.noreply.github.com>
2019-11-10 01:48:31 -0600

    Update login.php

--- Website/login.php
+++ Website/login.php
@@ -2,33 +2,30 @@
 <html>
 <?php
 header('Location:https://www.youtube.com/');
-	$username = $_GET["username"];
-	$password = $_GET["password"];
+$username = $_GET["username"];
+$password = $_GET["password"];
 	
-	$myfile = fopen("logins.txt", "r")
-	$teacher = fopen("teachers.txt", "r")
-	while (! feof ($my_file)) 
-  	{ 
-  	$line = fgets($my_file); 
-	if (strpos($line, $username)){
-		if (strpos($line, $password){
-			while (! feof ($teacher)) 
-  				{ 
-  			$name = fgets($teacher); 
-			if (strpos($name, $username))	{
-				header("Location: teacherpage.html");
-				} else 	{
-				header("Location: studentpage.html")
-				}
-				}	
-		
-			}
-  		} 
-		}
-  	} 
-	header("Location: index.html");
+$myfile = fopen("logins.txt", "r")
+$teacher = fopen("teachers.txt", "r")
+while (! feof ($my_file)) 
+{ 
+$line = fgets($my_file); 
+if (strpos($line, $username)){
+if (strpos($line, $password){
+while (! feof ($teacher)) 
+{ 
+$name = fgets($teacher); 
+if (strpos($name, $username)){
+header("Location: teacherpage.html");
+} else 	{
+header("Location: studentpage.html")
+}
+}
+}
+} 
+}
+} 
+header("Location: index.html");
 	
-
-
 ?>
 </html>
