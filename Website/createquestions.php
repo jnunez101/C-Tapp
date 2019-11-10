993e4489006e53393410c1c06d88a001abb80cf9
lloe <56943946+lloe@users.noreply.github.com>
2019-11-10 04:08:39 -0600

    questions

---
+++ Website/createquestions.php
@@ -0,0 +1,26 @@
+
+<?php
+$question = $_GET["question"];
+$answera = $_GET["answera"];
+$answerb = $_GET["answerb"];
+$answerc = $_GET["answerc"];
+$answerd = $_GET["answerd"];
+$answere = $_GET["answere"];
+$answer = $_GET["answer"];
+
+$questions = fopen("questions.txt", "a");
+fwrite($questions,$question);
+fwrite($questions,$answera);
+fwrite($questions,$answerb);
+fwrite($questions,$answerc);
+fwrite($questions,$answerd);
+fwrite($questions,$answere);
+
+$key = fopen("key.txt", "a");
+fwrite($key,$answer);
+
+fclose($questions);
+fclose($key);
+
+header("location:teacherpage.html");
+?>
