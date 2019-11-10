43afdf1cc1a11ef099153d995fddd42885997202
lloe <56943946+lloe@users.noreply.github.com>
2019-11-10 05:32:52 -0600

    all the changes

---
+++ Website/startquiz.php
@@ -0,0 +1,14 @@
+<?php
+$questions = fopen("questions.txt", "a");
+$stuquestions = fopen("stuquestions.txt", "a");
+$newline="\n";
+while(! feof($questions)){
+    $line=fgets($questions);
+    fwrite($stuquestions,$answera);
+    fwrite($stuquestions,$newline);
+}
+
+fclose($questions);
+fclose($stuquestions);
+header("location:teacherpage.html");
+?>
