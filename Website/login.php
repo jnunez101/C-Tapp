4b8a115b98eb7ce946c223060d296a169237ed82
lloe <56943946+lloe@users.noreply.github.com>
2019-11-10 02:15:05 -0600

    login maybe?

--- Website/login.php
+++ Website/login.php
@@ -4,28 +4,26 @@
 header('Location:https://www.youtube.com/');
 $username = $_GET["username"];
 $password = $_GET["password"];
-	
-$myfile = fopen("logins.txt", "r")
-$teacher = fopen("teachers.txt", "r")
-while (! feof ($my_file)) 
-{ 
-$line = fgets($my_file); 
-if (strpos($line, $username)){
-if (strpos($line, $password){
-while (! feof ($teacher)) 
-{ 
-$name = fgets($teacher); 
-if (strpos($name, $username)){
-header("Location: teacherpage.html");
-} else 	{
-header("Location: studentpage.html")
+
+$my_file = fopen("logins.txt", "r");
+$teacher = fopen("teachers.txt", "r");
+while (! feof($my_file)) {
+    $line = fgets($my_file);
+    if (strpos($line, $username)){
+        if (strpos($line, $password)){
+            while (! feof($teacher)) {
+            $name = fgets($teacher);
+            if (strpos($name, $username)){
+                header("Location: teacherpage.html");
+            } else 	{
+                header("Location: studentpage.html");
+            }
+            }
+        }
+    }
 }
-}
-}
-} 
-}
-} 
+
 header("Location: index.html");
-	
+
 ?>
 </html>
