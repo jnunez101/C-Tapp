71ea4947a689e7e604d63432400bb5c03db46eee
lloe <56943946+lloe@users.noreply.github.com>
2019-11-10 03:35:36 -0600

    Update login.php

--- Website/login.php
+++ Website/login.php
@@ -6,12 +6,12 @@ $password = $_GET["password"];
 
 $my_file = fopen("logins.txt", "r");
 $teacher = fopen("teachers.txt", "r");
-if($username = "username"){
+if($username == "username"){
     if($password = "password"){
         header("location:studentpage.html");
     }
 }
-if($username = "teacher"){
+if($username == "teacher"){
     if($password = "password"){
         header("location:teacherpage.html");
     }
