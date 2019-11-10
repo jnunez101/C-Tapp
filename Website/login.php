18d6874f235c07099db8010503b9b048d1886e68
lloe <56943946+lloe@users.noreply.github.com>
2019-11-10 00:56:22 -0600

    login

---
+++ Website/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+are you here?
+<?php
+	$username = $_GET["username"];
+	$password = $_GET["password"];
+
+	$myfile = fopen("logins.txt", "r")
+	$teacher = fopen("teachers.txt", "r")
+	while (! feof ($my_file)) 
+  	{ 
+  	$line = fgets($my_file); 
+	if (strpos($line, $username)){
+		if (strpos($line, $password){
+			while (! feof ($teacher)) 
+  				{ 
+  			$name = fgets($teacher); 
+			if (strpos($name, $username))	{
+				header("Location: teacherpage.html");
+				} else 	{
+				header("Location: studentpage.html")
+				}
+				}	
+		
+			}
+  		} 
+		}
+  	} 
+	
+
+
+?>
+</body>
+</html>
