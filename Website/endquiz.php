77d835e3cfd92a1b074f3d4676fd0413a907c489
lloe <56943946+lloe@users.noreply.github.com>
2019-11-10 11:16:42 -0600

    Update endquiz.php

--- Website/endquiz.php
+++ Website/endquiz.php
@@ -3,7 +3,8 @@ unlink("questions.txt");
 unlink("stuquestions.txt");
 $command='python3 Grades.py';
 exec($command,$out, $status);
-header("location:teacherpage.php")
 unlink("test.txt)
 unlinck("key.txt")
+header("location:teacherpage.php")
+
 ?>
